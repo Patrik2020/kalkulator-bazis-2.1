9fe5fd7987a8b8d5442bc9166a30761d4d4eff3f
Kovács Patrik <65179610+Patrik2020@users.noreply.github.com>
2026-07-05 20:45:01 +0200

    Wise ajánlók globális betöltése

--- js/global-head.js
+++ js/global-head.js
@@ -39,8 +39,10 @@ const basePath = `${projectRoot}/favicon`;
 const themeCssPath = `${projectRoot}/css/theme.css`;
 const footerCssPath = `${projectRoot}/css/layout/footer.css`;
 const cookieCssPath = `${projectRoot}/css/components/cookie.css`;
+const wisePromoCssPath = `${projectRoot}/css/components/wise-promo.css`;
 const themeScriptPath = `${projectRoot}/js/theme.js`;
 const pwaScriptPath = `${projectRoot}/js/pwa.js`;
+const wisePromoScriptPath = `${projectRoot}/js/wise-promo.js`;
 const calculatorCssPath = `${projectRoot}/css/pages/calculator-suite.css`;
 const calculatorScriptPath = `${projectRoot}/js/calculator-suite.js`;
 const normalizedPath = window.location.pathname.replace(/\/+$/, "");
@@ -98,6 +100,7 @@ const hasMainStylesheet = () =>
 ].forEach((attributes) => appendElement("link", attributes));
 
 appendElement("link", { rel: "stylesheet", href: themeCssPath });
+appendElement("link", { rel: "stylesheet", href: `${wisePromoCssPath}?v=20260705-1` });
 
 if (!hasMainStylesheet()) {
   appendElement("link", { rel: "stylesheet", href: footerCssPath });
@@ -111,6 +114,7 @@ if (isHomePage) {
 
 appendElement("script", { src: themeScriptPath, defer: "" });
 appendElement("script", { src: pwaScriptPath, defer: "" });
+appendElement("script", { src: `${wisePromoScriptPath}?v=20260705-1`, defer: "" });
 
 [
   { name: "application-name", content: "KalkulátorBázis" },
